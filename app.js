aff01a8ba830f6978c1068eac4d4941d857af4c0
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-16 00:37:26 +0300

    stored more complex data with JSON and used stringify to convert it into string

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const storeBtn = document.getElementById('store-btn');
 const retrBtn = document.getElementById('retrieve-btn');
 
 const userId = 'u123';
+const user = {
+  name: 'Max',
+  age: 30,
+  hobbies: ['Sports', 'Cooking']
+};
 
 storeBtn.addEventListener('click', () => {
   localStorage.setItem('uid', userId);
+  localStorage.setItem('user', JSON.stringify(user)); // we can convert this to JSON because JSON actually is a string "{}"
 });
 
 retrBtn.addEventListener('click', () => {
@@ -12,6 +18,8 @@ retrBtn.addEventListener('click', () => {
   // it's a synchronous action so it's actually not asynchronous as you could think,
   // you don't need a promise or a callback here,
   // this synchronously access the storage and gives you back a data immediately
+  const extractedUser = JSON.parse(localStorage.getItem('user'));
+  console.log(extractedUser);
   if (extractedId) {
     console.log('Got the Id - ' + extractedId);
   } else {
