683c22b448cb43bca6b5d2cec8ed4105ada36769
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-16 08:29:38 +0300

    explanation of cookies in great detail

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-// console.log(document.cookie);
-
 const storeBtn = document.getElementById('store-btn');
 const retrBtn = document.getElementById('retrieve-btn');
 
 storeBtn.addEventListener('click', () => {
   const userId = 'u123';
-  document.cookie = `uid=${userId}`;
-  // Under the hood, this simply uses a setter function which triggers when you assign a new value
-  // and that setter function will then add this as a new key-value pair to the existing pairs instead of replace the existing ones.
+  const user = {
+    name: 'Max',
+    age: 30
+  };
+  document.cookie = `uid=${userId}; max-age=360`;
+  // document.cookie = `uid=${userId}; expires=`;
+  document.cookie = `user=${JSON.stringify(user)}`;
 });
-// unlike local and session storage, cookies really only are available
-// if your web page is getting served with a real server
-// and the file protocol simply just doesn't simulate real circumstances under
-// which our page would be served
 
 retrBtn.addEventListener('click', () => {
   console.log(document.cookie);
+  // console.log(document.cookie.split(';')); // extra white space
+  const cookieData = document.cookie.split(';');
+  const data = cookieData.map(i => {
+    return i.trim();
+  });
+  // console.log(data);
+  console.log(data[1].split('=')[1]); // user value
+  // console.log(data.includes().split('=')[1]); // the best way to get data is to search it with the key name
 });
+
+/*
+
+The advantages of cookies are that you can set them to expire
+and that you can also send them to a server with requests.
+
+*/
