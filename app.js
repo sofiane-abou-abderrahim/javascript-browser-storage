6ab7b62c3dc33327241d4db95a6afa92db125845
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-16 12:05:29 +0300

    stored and retrieved an object on click event listeners

--- app.js
+++ app.js
@@ -1,40 +1,71 @@
 const storeBtn = document.getElementById('store-btn');
 const retrBtn = document.getElementById('retrieve-btn');
 
-// 1. The first step with IndexedDB is that you create a database or open a connection to an existing one.
-const dbRequest = indexedDB.open('StorageDummy', 1); // this is not promise based, instead this "open" method here returns a so-called request
+// 2. making the db connection global
+let db;
 
-// 2. Getting access to the database through that event object and configuring it
+const dbRequest = indexedDB.open('StorageDummy', 1);
+
+// 3. created onsuccess method
+dbRequest.onsuccess = function (event) {
+  db = event.target.result;
+};
+
 dbRequest.onupgradeneeded = function (event) {
-  const db = event.target.result;
+  db = event.target.result;
 
   const objStore = db.createObjectStore('products', { keyPath: 'id' });
 
   objStore.transaction.oncomplete = function (event) {
     const productStore = db
       .transaction('products', 'readwrite')
-      .objectStore('products'); // gives direct access to that object store we're trying to establish a connection to
+      .objectStore('products');
     productStore.add({
-      id: 'p1', // you need to have your key path here in the object
+      id: 'p1',
       title: 'A First Product',
       price: 12.99,
       tags: ['Expensive', 'Luxury']
     });
-  }; // initializing the object store
+  };
 };
 
-/*
-
-This is more structured data stored in there without the need to encode it with JSON or anything like that
-and of course you can store multiple such objects with ease here, all identified through their key
-
-*/
-
-// 3. Handling errors
 dbRequest.onerror = function (event) {
   console.log('ERROR!');
 };
 
-storeBtn.addEventListener('click', () => {});
+// 1. storing the object inside the event listener
+storeBtn.addEventListener('click', () => {
+  if (!db) {
+    return;
+  }
+  const productStore = db
+    .transaction('products', 'readwrite')
+    .objectStore('products');
+  productStore.add({
+    id: 'p2',
+    title: 'A Second Product',
+    price: 122.99,
+    tags: ['Expensive', 'Luxury']
+  });
+});
 
-retrBtn.addEventListener('click', () => {});
+// 4. retrieving the object
+retrBtn.addEventListener('click', () => {
+  const productStore = db
+    .transaction('products', 'readwrite')
+    .objectStore('products');
+  const request = productStore.get('p2');
+
+  request.onsuccess = function () {
+    console.log(request.result);
+  };
+});
+
+/*
+
+So some aspects which can be confusing for sure, it is the API IndexedDB offers,
+though because it's a bit clunky with all these handler assignments here,
+there actually are third-party libraries,
+most importantly idb.js
+
+*/
